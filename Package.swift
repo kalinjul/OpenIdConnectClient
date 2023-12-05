931b0808eb1c73bbe92126299bc9745fdf9b29a2
Julian Kalinowski <Julian.Kalinowski@dataport.de>
2023-12-05 09:08:25 +0100

    fix url

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OpenIdConnectClient",
-            url: "https://raw.githubusercontent.com/kalinjul/OpenIdConnectClient/0.3.0/OpenIdConnectClient.zip",
+            url: "https://github.com/kalinjul/OpenIdConnectClient/raw/0.3.0/OpenIdConnectClient.zip",
             checksum: "0801a0cfe5ec140f8abfd47213543b4614111e48e462ee18bf6f8cbacdc014e2"
         ),
     ]
