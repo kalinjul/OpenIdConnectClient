8a951d346955a6d087ca1b633b38e48371da06a4
Julian Kalinowski <kalinjul@users.noreply.github.com>
2025-07-04 08:20:40 +0000

    New iOS package (snapshot)

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "OpenIdConnectClient",
             url: "https://github.com/kalinjul/OpenIdConnectClient/releases/download/main/OpenIdConnectClient.zip",
-            checksum: "32b9e810abf47695b2d5299c5eaf80d85b2817567388aa47996ec1270adb9ceb"
+            checksum: "279a5052de1e749fd1c8f1cd27d06228307331d7f666da00ea2d0ae207d78787"
         ),
     ]
 )
